57f770d9b1940a2039a738952d1121b6c0501549
Salman Mustafa <salman.m@vettons.com>
2020-09-30 13:07:51 +0800

    added latest query and updated readme

--- README.md
+++ README.md
@@ -47,6 +47,7 @@
 ```bash
 npm install && npm start
 ```
+
 ### Steps Explained
 
 - Open terminal and run `npm install`
@@ -59,14 +60,26 @@ npm install && npm start
 
 - Run `npm run server` for local development
 
-
 ## Queries
 
 #### `GET` overall COVID-19 stats
 
 ```graphql
-{ 
+{
   total {
+    active
+    confirmed
+    deaths
+    recovered
+  }
+}
+```
+
+#### `GET` latest COVID-19 stats
+
+```graphql
+{
+  latest {
     active
     confirmed
     deaths
@@ -100,8 +113,8 @@ npm install && npm start
     dailyrecovered
   }
 }
-
 ```
+
 ## Dependencies Used
 
 - [express](https://www.npmjs.com/package/express): A web application framework for [Node.js](https://nodejs.org/)
@@ -113,23 +126,24 @@ npm install && npm start
 ## Related Work
 
 - [COVID-19 Tracker - INDIA](http://corona-cases-india.netlify.com/) - A React App with the following features:
-    - Graphs showing daily **confirmed**, **recovered** and **deceased** cases
-    - **State-wise** report presented in Map of India
-    - Tabular data for global corona cases
-    - **Latest news** about COVID-19
-    - Additional resources such as symptoms, precautions etc.
 
-- [India Fights Corona -  Get current COVID-19 stats](https://indiafightscorona.netlify.app/)!
+  - Graphs showing daily **confirmed**, **recovered** and **deceased** cases
+  - **State-wise** report presented in Map of India
+  - Tabular data for global corona cases
+  - **Latest news** about COVID-19
+  - Additional resources such as symptoms, precautions etc.
+
+- [India Fights Corona - Get current COVID-19 stats](https://indiafightscorona.netlify.app/)!
 
 - [COVID-19 Tracker - Homepage](https://vinitshahdeo.github.io/COVID19/)
 
-- An [Open Letter](https://github.com/vinitshahdeo/Water-Monitoring-System/issues/236) to all the contributors -  [STAY HOME, STAY SAFE!](https://github.com/vinitshahdeo/COVID19)
+- An [Open Letter](https://github.com/vinitshahdeo/Water-Monitoring-System/issues/236) to all the contributors - [STAY HOME, STAY SAFE!](https://github.com/vinitshahdeo/COVID19)
 
 ## License
 
 [![FOSSA Status](https://app.fossa.com/api/projects/git%2Bgithub.com%2Fvinitshahdeo%2Fcovid19api.svg?type=large)](https://app.fossa.com/projects/git%2Bgithub.com%2Fvinitshahdeo%2Fcovid19api?ref=badge_large)
 
-## `< />` with ♡ by 
+## `< />` with ♡ by
 
 [![](https://sourcerer.io/fame/vinitshahdeo/vinitshahdeo/COVID19/images/0)](https://fayz.in/stories/s/1522/0/?ckt_id=ZGL1ZGVk&title=story_of_vinit_shahdeo)[![](https://sourcerer.io/fame/vinitshahdeo/vinitshahdeo/COVID19/images/1)](https://indiafightscorona.netlify.app/)[![](https://sourcerer.io/fame/vinitshahdeo/vinitshahdeo/COVID19/images/2)](https://github.com/vinitshahdeo/COVID19)[![](https://sourcerer.io/fame/vinitshahdeo/vinitshahdeo/COVID19/images/3)](https://www.linkedin.com/in/vinitshahdeo/)[![](https://sourcerer.io/fame/vinitshahdeo/vinitshahdeo/COVID19/images/4)](http://corona-cases-india.netlify.com/)[![](https://sourcerer.io/fame/vinitshahdeo/vinitshahdeo/COVID19/images/5)](http://corona-cases-india.netlify.com/)[![](https://sourcerer.io/fame/vinitshahdeo/vinitshahdeo/COVID19/images/6)](https://github.com/vinitshahdeo)[![](https://sourcerer.io/fame/vinitshahdeo/vinitshahdeo/COVID19/images/7)](https://fayz.in/stories/s/1522/0/?ckt_id=ZGL1ZGVk&title=story_of_vinit_shahdeo)
 
@@ -139,18 +153,16 @@ npm install && npm start
 
 ## Leave a :star:
 
-
 Check out **@vinitshahdeo/[COVID19](https://github.com/vinitshahdeo/COVID19)** repository. **Consider leaving a star!** :heart: :hugs:
 
+---
 
-----
 ```javascript
-
 if (_.isAwesome(thisRepo)) {
   thisRepo.star(); // thanks in advance :p
 }
-
 ```
-----
 
-[![GitHub followers](https://img.shields.io/github/followers/vinitshahdeo.svg?label=Follow%20@vinitshahdeo&style=social)](https://github.com/vinitshahdeo/)  [![Twitter Follow](https://img.shields.io/twitter/follow/Vinit_Shahdeo?style=social)](https://twitter.com/Vinit_Shahdeo)
+---
+
+[![GitHub followers](https://img.shields.io/github/followers/vinitshahdeo.svg?label=Follow%20@vinitshahdeo&style=social)](https://github.com/vinitshahdeo/) [![Twitter Follow](https://img.shields.io/twitter/follow/Vinit_Shahdeo?style=social)](https://twitter.com/Vinit_Shahdeo)
--- src/schema.js
+++ src/schema.js
@@ -1,104 +1,118 @@
-const axios = require('axios').default;
+const axios = require("axios").default;
 
 const {
-    GraphQLSchema,
-    GraphQLString,
-    GraphQLObjectType,
-    GraphQLList,
-    GraphQLInt
-} = require('graphql');
-
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLInt,
+} = require("graphql");
 
 const CovidDataType = new GraphQLObjectType({
-    name: 'CovidStats',
-    fields: () => ({
-        active: {
-            type: GraphQLInt
-        },
-        confirmed: {
-            type: GraphQLString
-        },
-        deaths: {
-            type: GraphQLString
-        },
-        recovered: {
-            type: GraphQLString
-        }
-    })
+  name: "CovidStats",
+  fields: () => ({
+    active: {
+      type: GraphQLInt,
+    },
+    confirmed: {
+      type: GraphQLString,
+    },
+    deaths: {
+      type: GraphQLString,
+    },
+    recovered: {
+      type: GraphQLString,
+    },
+  }),
 });
 
 const StateCovidDataType = new GraphQLObjectType({
-    name: 'statewise',
-    fields: {
-        state: {
-            type: GraphQLString
-        },
-        active: {
-            type: GraphQLString
-        },
-        confirmed: {
-            type: GraphQLString
-        },
-        deaths: {
-            type: GraphQLString
-        },
-        recovered: {
-            type: GraphQLString
-        }
-    }
+  name: "statewise",
+  fields: {
+    state: {
+      type: GraphQLString,
+    },
+    active: {
+      type: GraphQLString,
+    },
+    confirmed: {
+      type: GraphQLString,
+    },
+    deaths: {
+      type: GraphQLString,
+    },
+    recovered: {
+      type: GraphQLString,
+    },
+  },
 });
 
 const DailyCovidDataType = new GraphQLObjectType({
-    name: 'daily',
-    fields: {
-        date: {
-            type: GraphQLString
-        },
-        dailyconfirmed: {
-            type: GraphQLInt
-        },
-        dailydeceased: {
-            type: GraphQLInt
-        },
-        dailyrecovered: {
-            type: GraphQLInt
-        }
-    }
+  name: "daily",
+  fields: {
+    date: {
+      type: GraphQLString,
+    },
+    dailyconfirmed: {
+      type: GraphQLInt,
+    },
+    dailydeceased: {
+      type: GraphQLInt,
+    },
+    dailyrecovered: {
+      type: GraphQLInt,
+    },
+  },
 });
 
 /**
  * Total data
  */
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        total: {
-            type: CovidDataType,
-            async resolve() {
-                const data = await axios.get('https://api.covid19india.org/data.json')
-                    .then(res => res.data.statewise[0]);
-                return data;
-            }
-        },
-        statewise: {
-            type: new GraphQLList(StateCovidDataType),
-            async resolve() {
-                const data = await axios.get('https://api.covid19india.org/data.json')
-                    .then(res => res.data.statewise.splice(1));
-                return data;
-            }
-        },
-        datewise: {
-            type: new GraphQLList(DailyCovidDataType),
-            async resolve() {
-                const data = await axios.get('https://api.covid19india.org/data.json')
-                    .then(res => res.data.cases_time_series);
-                return data;
-            }
-        }
-    }
+  name: "RootQueryType",
+  fields: {
+    total: {
+      type: CovidDataType,
+      async resolve() {
+        const data = await axios
+          .get("https://api.covid19india.org/data.json")
+          .then((res) => res.data.statewise[0]);
+        return data;
+      },
+    },
+    statewise: {
+      type: new GraphQLList(StateCovidDataType),
+      args: {
+        stateName: { type: GraphQLString },
+      },
+      async resolve(obj, args, context, info) {
+        const data = await axios
+          .get("https://api.covid19india.org/data.json")
+          .then((res) => res.data.statewise.splice(1));
+        return data;
+      },
+    },
+    datewise: {
+      type: new GraphQLList(DailyCovidDataType),
+      async resolve() {
+        const data = await axios
+          .get("https://api.covid19india.org/data.json")
+          .then((res) => res.data.cases_time_series);
+        return data;
+      },
+    },
+    latest: {
+      type: CovidDataType,
+      async resolve() {
+        const data = await axios
+          .get("https://api.covid19india.org/data.json")
+          .then((res) => res.data.statewise[0]);
+        return data;
+      },
+    },
+  },
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+  query: RootQuery,
 });
