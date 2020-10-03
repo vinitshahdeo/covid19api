2dd7bc98d9c08d66dde3006f69c363435610c49c
BrayanAbraham <brayanabraham@gmail.com>
2020-10-03 11:47:10 +0530

    State Code wise data collection and README update

--- src/schema.js
+++ src/schema.js
@@ -1,104 +1,133 @@
-const axios = require('axios').default;
+const axios = require("axios").default;
 
 const {
-    GraphQLSchema,
-    GraphQLString,
-    GraphQLObjectType,
-    GraphQLList,
-    GraphQLInt
-} = require('graphql');
-
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLInt,
+} = require("graphql");
 
 const CovidDataType = new GraphQLObjectType({
-    name: 'CovidStats',
-    fields: () => ({
-        active: {
-            type: GraphQLInt
-        },
-        confirmed: {
-            type: GraphQLString
-        },
-        deaths: {
-            type: GraphQLString
-        },
-        recovered: {
-            type: GraphQLString
-        }
-    })
+  name: "CovidStats",
+  fields: () => ({
+    active: {
+      type: GraphQLInt,
+    },
+    confirmed: {
+      type: GraphQLString,
+    },
+    deaths: {
+      type: GraphQLString,
+    },
+    recovered: {
+      type: GraphQLString,
+    },
+  }),
 });
 
 const StateCovidDataType = new GraphQLObjectType({
-    name: 'statewise',
-    fields: {
-        state: {
-            type: GraphQLString
-        },
-        active: {
-            type: GraphQLString
-        },
-        confirmed: {
-            type: GraphQLString
-        },
-        deaths: {
-            type: GraphQLString
-        },
-        recovered: {
-            type: GraphQLString
-        }
-    }
+  name: "statewise",
+  fields: {
+    state: {
+      type: GraphQLString,
+    },
+    active: {
+      type: GraphQLString,
+    },
+    confirmed: {
+      type: GraphQLString,
+    },
+    deaths: {
+      type: GraphQLString,
+    },
+    recovered: {
+      type: GraphQLString,
+    },
+  },
 });
 
 const DailyCovidDataType = new GraphQLObjectType({
-    name: 'daily',
-    fields: {
-        date: {
-            type: GraphQLString
-        },
-        dailyconfirmed: {
-            type: GraphQLInt
-        },
-        dailydeceased: {
-            type: GraphQLInt
-        },
-        dailyrecovered: {
-            type: GraphQLInt
-        }
-    }
+  name: "daily",
+  fields: {
+    date: {
+      type: GraphQLString,
+    },
+    dailyconfirmed: {
+      type: GraphQLInt,
+    },
+    dailydeceased: {
+      type: GraphQLInt,
+    },
+    dailyrecovered: {
+      type: GraphQLInt,
+    },
+  },
+});
+
+const StateCodeCovidDataType = new GraphQLObjectType({
+  name: "statecodewise",
+  fields: {
+    statecode: {
+      type: GraphQLString,
+    },
+    active: {
+      type: GraphQLString,
+    },
+    confirmed: {
+      type: GraphQLString,
+    },
+    deaths: {
+      type: GraphQLString,
+    },
+  },
 });
 
 /**
  * Total data
  */
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        total: {
-            type: CovidDataType,
-            async resolve() {
-                const data = await axios.get('https://api.covid19india.org/data.json')
-                    .then(res => res.data.statewise[0]);
-                return data;
-            }
-        },
-        statewise: {
-            type: new GraphQLList(StateCovidDataType),
-            async resolve() {
-                const data = await axios.get('https://api.covid19india.org/data.json')
-                    .then(res => res.data.statewise.splice(1));
-                return data;
-            }
-        },
-        datewise: {
-            type: new GraphQLList(DailyCovidDataType),
-            async resolve() {
-                const data = await axios.get('https://api.covid19india.org/data.json')
-                    .then(res => res.data.cases_time_series);
-                return data;
-            }
-        }
-    }
+  name: "RootQueryType",
+  fields: {
+    total: {
+      type: CovidDataType,
+      async resolve() {
+        const data = await axios
+          .get("https://api.covid19india.org/data.json")
+          .then((res) => res.data.statewise[0]);
+        return data;
+      },
+    },
+    statewise: {
+      type: new GraphQLList(StateCovidDataType),
+      async resolve() {
+        const data = await axios
+          .get("https://api.covid19india.org/data.json")
+          .then((res) => res.data.statewise.splice(1));
+        return data;
+      },
+    },
+    datewise: {
+      type: new GraphQLList(DailyCovidDataType),
+      async resolve() {
+        const data = await axios
+          .get("https://api.covid19india.org/data.json")
+          .then((res) => res.data.cases_time_series);
+        return data;
+      },
+    },
+    statecodewise: {
+      type: new GraphQLList(StateCodeCovidDataType),
+      async resolve() {
+        const data = await axios
+          .get("https://api.covid19india.org/data.json")
+          .then((res) => res.data.statewise.splice(1));
+        return data;
+      },
+    },
+  },
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+  query: RootQuery,
 });
